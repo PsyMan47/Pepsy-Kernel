764742346ba8a107235ecaf64190b2c9b9344145
PsyMan47 <xhonirexhepi@gmail.com>
2018-01-30 09:56:40 +0000

    Add build scripts, AnyKernel2 and dtbTool

---
+++ builder-minote2.sh
@@ -0,0 +1,33 @@
+KERNEL_DIR=$PWD
+ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel2
+DTBTOOL=$KERNEL_DIR/dtbTool
+CCACHEDIR=../CCACHE/scorpio
+TOOLCHAINDIR=~/toolchain/aarch64-linux-android-4.9
+DATE=$(date +"%d%m%Y")
+KERNEL_NAME="Pepsy-Kernel"
+DEVICE="-scorpio-"
+VER="-v0.1-"
+TYPE="O-MR1"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
+
+rm $ANYKERNEL_DIR/scorpio/zImage $ANYKERNEL_DIR/scorpio/dtb
+rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
+
+export ARCH=arm64
+export KBUILD_BUILD_USER="Psy_Man"
+export KBUILD_BUILD_HOST="PsyBuntu"
+export CROSS_COMPILE=$TOOLCHAINDIR/bin/aarch64-linux-android-
+export LD_LIBRARY_PATH=$TOOLCHAINDIR/lib/
+export USE_CCACHE=1
+export CCACHE_DIR=$CCACHEDIR/.ccache
+
+make clean && make mrproper
+make scorpio_defconfig
+make -j$( nproc --all )
+
+./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
+cp $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/scorpio/dtb
+cp $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/scorpio/zImage
+cd $ANYKERNEL_DIR/scorpio
+zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
+
