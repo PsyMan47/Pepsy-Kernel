4d1b31d14e4f91de4bf9b8c59d4243470b1bd2ea
PsyMan47 <xhonirexhepi@gmail.com>
2018-04-07 18:49:57 +0000

    Stop building separated dtb image - Append dtbs in Image.gz-dtb instead by adjusting dts path

--- builder-mimix.sh
+++ builder-mimix.sh
@@ -1,17 +1,16 @@
 KERNEL_DIR=$PWD
 ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel2
-DTBTOOL=$KERNEL_DIR/dtbTool
 CCACHEDIR=../CCACHE/lithium
 TOOLCHAINDIR=~/toolchain/aarch64-linux-android-4.9
 DATE=$(date +"%d%m%Y")
 KERNEL_NAME="Pepsy-Kernel"
 DEVICE="-lithium-"
-VER="-v0.1-"
-TYPE="O-MR1"
+VER="-v0.1"
+TYPE="-O-MR1"
 FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
 
-rm $ANYKERNEL_DIR/lithium/zImage $ANYKERNEL_DIR/lithium/dtb
-rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
+rm $ANYKERNEL_DIR/lithium/Image.gz-dtb
+rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
 
 export ARCH=arm64
 export KBUILD_BUILD_USER="Psy_Man"
@@ -25,8 +24,6 @@ make clean && make mrproper
 make lithium_defconfig
 make -j$( nproc --all )
 
-./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
-cp $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/lithium/dtb
-cp $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/lithium/zImage
+cp $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/lithium
 cd $ANYKERNEL_DIR/lithium/
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
