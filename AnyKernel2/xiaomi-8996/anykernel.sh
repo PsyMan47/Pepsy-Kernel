5809387767514dcde740a89919949807812f7064
PsyMan47 <xhonirexhepi@gmail.com>
2018-07-17 14:45:26 +0000

    Unify devicetrees and defconfigs

---
+++ AnyKernel2/xiaomi-8996/anykernel.sh
@@ -0,0 +1,39 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=null
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=capricorn
+device.name2=gemini
+device.name3=lithium
+device.name4=natrium
+device.name5=scorpio
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+
+## end install
+
---
+++ xiaomi-8996.sh
@@ -0,0 +1,29 @@
+KERNEL_DIR=$PWD
+ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel2
+CCACHEDIR=../CCACHE/xiaomi-8996
+TOOLCHAINDIR=~/toolchain/aarch64-linux-android-4.9
+DATE=$(date +"%d%m%Y")
+KERNEL_NAME="Pepsy-Kernel"
+DEVICE="-Xiaomi-8996-"
+VER="-v0.1"
+TYPE="-O-MR1"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
+
+rm $ANYKERNEL_DIR/xiaomi-8996/Image.gz-dtb
+rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
+
+export ARCH=arm64
+export KBUILD_BUILD_USER="Psy_Man"
+export KBUILD_BUILD_HOST="PsyBuntu"
+export CROSS_COMPILE=$TOOLCHAINDIR/bin/aarch64-linux-android-
+export LD_LIBRARY_PATH=$TOOLCHAINDIR/lib/
+export USE_CCACHE=1
+export CCACHE_DIR=$CCACHEDIR/.ccache
+
+make clean && make mrproper
+make msm8996_defconfig
+make -j$( nproc --all )
+
+cp $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/xiaomi-8996
+cd $ANYKERNEL_DIR/xiaomi-8996
+zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
