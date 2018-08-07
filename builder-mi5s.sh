d602c3cf5ae7404ab1b754e1b0df680672f6b698
PsyMan47 <xhonirexhepi@gmail.com>
2018-08-07 00:29:05 +0000

    wercker: Build and upload kernels separately

--- builder-mi5s.sh
+++ builder-mi5s.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 KERNEL_DIR=$PWD
 ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel2
 CCACHEDIR=../CCACHE/capricorn
-TOOLCHAINDIR=~/toolchain/aarch64-linux-android-4.9
+TOOLCHAINDIR=/pipeline/build/root/toolchain/aarch64-linux-android-4.9
 DATE=$(date +"%d%m%Y")
-KERNEL_NAME="Pepsy-Kernel"
+KERNEL_NAME="Pepsy"
 DEVICE="-capricorn-"
 VER="-v0.1"
 TYPE="-O-MR1"
@@ -24,6 +26,16 @@ make clean && make mrproper
 make capricorn_defconfig
 make -j$( nproc --all )
 
+{
 cp $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/capricorn
+} || {
+  if [ $? != 0 ]; then
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build failed for capricorn :c" -d chat_id=@pepsykernel;
+    exit
+  fi
+}
+
 cd $ANYKERNEL_DIR/capricorn
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
+curl -F chat_id="-1001152658251" -F document=@"$FINAL_ZIP" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+mv $FINAL_ZIP /pipeline/output/$FINAL_ZIP
--- builder-mimix.sh
+++ builder-mimix.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 KERNEL_DIR=$PWD
 ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel2
 CCACHEDIR=../CCACHE/lithium
-TOOLCHAINDIR=~/toolchain/aarch64-linux-android-4.9
+TOOLCHAINDIR=/pipeline/build/root/toolchain/aarch64-linux-android-4.9
 DATE=$(date +"%d%m%Y")
-KERNEL_NAME="Pepsy-Kernel"
+KERNEL_NAME="Pepsy"
 DEVICE="-lithium-"
 VER="-v0.1"
 TYPE="-O-MR1"
@@ -24,6 +26,16 @@ make clean && make mrproper
 make lithium_defconfig
 make -j$( nproc --all )
 
+{
 cp $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/lithium
+} || {
+  if [ $? != 0 ]; then
+    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build failed for lithium :c" -d chat_id=@pepsykernel;
+    exit
+  fi
+}
+
 cd $ANYKERNEL_DIR/lithium/
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
+curl -F chat_id="-1001152658251" -F document=@"$FINAL_ZIP" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+mv $FINAL_ZIP /pipeline/output/$FINAL_ZIP
